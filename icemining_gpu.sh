5d0dd8c60343e6cd69bb6f20ea4a5b98560c1149
bvndls <bvndls@mba.home.arpa>
2024-05-23 02:53:14 +0200

    +icemining, ton_ninja cfg

---
+++ icemining_gpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Assign a name for this worker:" WORKER
+echo "The name assigned to the worker is:" $WORKER
+
+apt install nano -y && \
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.88/lolMiner_v1.88_Lin64.tar.gz && \
+tar -xvf lolMiner_v1.88_Lin64.tar.gz && \
+cd 1.88 && \
+echo '#!/bin/bash
+
+POOL=stratum+tcp://de.gpu.hashrate.to:4002
+WALLET=gpu:UQAt9tz1pgMZaE2SZmpO7xNKh-RbanYPxBqRdM9_xpBSX7IQ.'$WORKER'
+
+cd "$(dirname "$0")"
+
+./lolMiner -a TON --pool $POOL --user $WALLET --apiport 4444 --ton-mode 6 $@' > minegpu.sh && \
+chmod +x minegpu.sh && \
+./minegpu.sh
